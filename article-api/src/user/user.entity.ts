6c04abb1083b1e3c397644954ee90b7c1b70783a
Valentin <valentin.soyer@epitech.eu>
2021-01-31 11:36:50 +0100

    fix(User): remove useless column

--- article-api/src/user/user.entity.ts
+++ article-api/src/user/user.entity.ts
@@ -21,11 +21,8 @@ export class User extends BaseEntity {
     @Exclude()
     salt: string;
 
-    @Column()
-    articles: string[]
-
     async validatePassword(password: string): Promise<boolean> {
-            const hash = await bcrypt.hash(password, this.salt);
-            return hash === this.password;
+        const hash = await bcrypt.hash(password, this.salt);
+        return hash === this.password;
     }
 }
